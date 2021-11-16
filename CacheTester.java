3a0f361a719d5bf302a9628090c25ae6cd56a6a2
stsingh <sahej3526@gmail.com>
2021-11-15 20:48:15 -0500

    bruhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- CacheTester.java
+++ CacheTester.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.*;
 public class CacheTester {
 	@Test
 	public void leastRecentlyUsedIsCorrect () {
-		final CapitalLoader provider = new CapitalLoader(); 
+		final CapitalLoader provider = new CapitalLoader(); //instantiate data provider
+		// add data to data provider
 		provider.put("Massachusetts", "Boston");
 		provider.put("Texas", "Austin");
 		provider.put("Wisconsin", "Cheyenne");
@@ -18,32 +19,35 @@ public class CacheTester {
 		provider.put("Vermont", "Montpelier");
 		provider.put("Connecticut", "Hartford");
 		provider.put("California", "Sacramento");
-		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5);
+		//instantiate LRU Cache
+		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5); 
+		//fill cache to capacity
 		cache.get("Massachusetts");
 		cache.get("Texas");
 		cache.get("Wisconsin");
 		cache.get("Maine");
 		cache.get("Rhode Island");
 		assertTrue(cache.isInCache("Massachusetts")); //assert that massachusetts was in the cache in the first place
+		//go over capacity of cache
 		cache.get("Vermont");
 		cache.get("Connecticut");
-		cache.get("California");
 		assertTrue(!cache.isInCache("Massachusetts"));//assert that massachusetts was evicted
 		assertTrue(!cache.isInCache("Texas"));//assert that texas was evicted (additional test)
-		assertTrue(cache.isInCache("Maine"));//assert that maine stays in the cache
 	}
 	@Test
 	public void cachingIsFunctional () {
-		final CapitalLoader provider = new CapitalLoader(); 
+		final CapitalLoader provider = new CapitalLoader(); //instantiate data provider
+		//add data to data provider
 		provider.put("Massachusetts", "Boston");
 		provider.put("Texas", "Austin");
-		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5);
+		//instantiate LRU Cache
+		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5); 
 		cache.get("Massachusetts");
 		// Test that the key has been cached
 		assertTrue(cache.isInCache("Massachusetts"));
 		cache.get("Massachusetts");
 		// Test that the cache fetched the cached key, rather than the provider
-		//assertTrue(provider.getNumFetches() == 1); //for our LRUCache only
+		assertTrue(provider.getNumFetches() == 1); 
 		// Change the key's value in the provider 
 		provider.put("Massachusetts", "Worcester");
 		// Test that the retrieved value is coming from the cache, rather than the provider
@@ -51,11 +55,14 @@ public class CacheTester {
 	}
 	@Test
 	public void numMissesIsFunctional() {
-		final CapitalLoader provider = new CapitalLoader(); 
+		final CapitalLoader provider = new CapitalLoader(); //instantiate data provider
+		// add data to data provider
 		provider.put("Massachusetts", "Boston");
 		provider.put("Texas", "Austin");
 		provider.put("Maine", "Augusta");
-		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5);
+		//instantiate the LRU Cache
+		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5); 
+		//call data through cache from data provider
 		cache.get("Massachusetts");
 		cache.get("Texas");
 		cache.get("Maine");
@@ -67,13 +74,14 @@ public class CacheTester {
 	}
 	@Test
 	public void isInCacheIsFunctional() {
-		final CapitalLoader provider = new CapitalLoader(); 
+		final CapitalLoader provider = new CapitalLoader(); //instantiate data provider
+		//instantiate the LRU Cache
 		final Cache<String,String> cache = new LRUCache<String,String> (provider, 5);
 		provider.put("Massachusetts", "Boston");
-		// key should not be in cache
+		// assert that the key does not get added to the cache when being added to the data provider
 		assertTrue(!cache.isInCache("Massachusetts"));
 		cache.get("Massachusetts");
-		// now, key should be cached
+		// assert that key has been cached after using dedicated method
 		assertTrue(cache.isInCache("Massachusetts"));
 
 	}
